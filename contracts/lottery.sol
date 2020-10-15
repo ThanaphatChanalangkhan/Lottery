7a7cc12d566438c1be14c4a78b2fa40b2b21db42
ThanaphatChanalangkhan <60050179@kmitl.ac.th>
2020-10-15 15:59:08 +0700

    111

---
+++ contracts/lottery.sol
@@ -0,0 +1,36 @@
+pragma solidity >=0.6.6 < 0.8.0;
+contract Lottery {
+    address public manager;
+    address [] public players;
+    address payable winner;
+
+    constructor() public {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require (msg.value >= 0.01 ether);
+        players.push(msg.sender);
+    }
+    function random() private view returns (uint) {
+        bytes memory val;
+        val = abi.encodePacked(block.difficulty, now, players);
+        return uint (keccak256(val));
+    }
+    function pickWinner() public checkForOnlyManager {
+        uint index = random() % players.length;
+        //address payable winner;
+        winner = payable (players[index]);
+        winner.transfer(address(this).balance);
+        //clear array for next round
+        players = new address[](0); 
+    } 
+     modifier checkForOnlyManager {
+        require (msg.sender == manager);
+        _;  
+    }
+    
+    function getPlayers() public view returns (address[] memory) {
+        return players;
+    }
+}
